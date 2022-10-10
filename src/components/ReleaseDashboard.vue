2a6dc65af2dc74e1d0c5a2c5ba5cf348c7da952c
Olivier Vernin <olivier.vernin@suse.com>
2022-10-10 20:27:40 +0200

    Add support for multiple projects
    Signed-off-by: Olivier Vernin <olivier.vernin@suse.com>

--- src/components/ReleaseDashboard.vue
+++ src/components/ReleaseDashboard.vue
@@ -3,75 +3,92 @@
       class="py-8 px-6"
       fluid
   >
-      <v-row class="mx-auto" v-if="appsData.description">
-        <v-col
-            cols="auto"
-            lg="12"
-            md="12"
-            sm="12"
-          >
-            <v-card
-              elevation="3"
-              shaped
-              outline
+
+      <v-row v-for="project in appsData.projects"
+              :key="project.name">
+          <v-col
+              cols="auto"
+              lg="12"
+              md="12"
+              sm="12"
+            >
+
+          <!-- Show Project Description -->
+          <v-row class="mx-auto" v-if="project.description">
+            <v-col
+                cols="auto"
+                lg="12"
+                md="12"
+                sm="12"
               >
-              <v-card-title align="center">Description</v-card-title>
-              <v-card-text align="center">
-                <p>{{ appsData.description }}</p>
-              </v-card-text>
-            </v-card>
+                <v-card
+                  elevation="3"
+                  shaped
+                  outline
+                  >
+                  <v-card-title align="center">{{ project.name }}</v-card-title>
+                  <v-card-text align="center">
+                    <p>{{ project.description }}</p>
+                  </v-card-text>
+                </v-card>
+            </v-col>
+          </v-row>
+
+
+          <!-- For each project, show apps information-->
+          <v-row class="mx-auto">
+              <v-col
+                  v-for="app in project.apps"
+                  :key="app.name"
+                  cols="auto"
+                  lg="4"
+                  md="12"
+                  sm="12"
+              >
+                <v-card
+                  elevation="3"
+                  shaped
+                  outline
+                  min-height="300"
+                  >
+                  <v-card-title align="center">{{ app.name}}</v-card-title>
+
+                  <v-row>
+                    <v-col>
+                      <v-card-text align="center">
+                        {{ app.description}}
+                        {{ isMatching(app.currentValue, app.expectedValue) }}
+                      </v-card-text>
+                    </v-col>
+                  </v-row>
+
+                    <v-row>
+                      <v-col
+                        cols="auto"
+                        lg="6"
+                        md="12"
+                        sm="12"
+                      >
+                        <v-card-title align="center">Current</v-card-title>
+                        <v-card-text align="center">{{ app.currentValue}}</v-card-text>
+                      </v-col>
+                      <v-col
+                        cols="auto"
+                        lg="6"
+                        md="12"
+                        sm="12"
+                      >
+                        <v-card-title align="center">Expected</v-card-title>
+                        <v-card-text align="center">{{ app.expectedValue}}</v-card-text>
+                      </v-col>
+                    </v-row>
+                </v-card>
+              </v-col>
+          </v-row>
+
         </v-col>
       </v-row>
 
-      <v-row class="mx-auto">
-          <v-col
-              v-for="app in appsData.apps"
-              :key="app.name"
-              cols="auto"
-              lg="4"
-              md="12"
-              sm="12"
-          >
-            <v-card
-              elevation="3"
-              shaped
-              outline
-              min-height="300"
-              >
-              <v-card-title align="center">{{ app.name}}</v-card-title>
-
-              <v-row>
-                <v-col>
-                  <v-card-text align="center">
-                    {{ app.description}}
-                    {{ isMatching(app.currentValue, app.expectedValue) }}
-                  </v-card-text>
-                </v-col>
-              </v-row>
-
-                <v-row>
-                  <v-col
-                    cols="auto"
-                    lg="6"
-                    md="12"
-                    sm="12"
-                  >
-                    <v-card-title align="center">Current</v-card-title>
-                    <v-card-text align="center">{{ app.currentValue}}</v-card-text>
-                  </v-col>
-                  <v-col
-                    cols="auto"
-                    lg="6"
-                    md="12"
-                    sm="12"
-                  >
-                    <v-card-title align="center">Expected</v-card-title>
-                    <v-card-text align="center">{{ app.expectedValue}}</v-card-text>
-                  </v-col>
-                </v-row>
-            </v-card>
-          </v-col>
-      </v-row>
   </v-container>
 </template>
 
