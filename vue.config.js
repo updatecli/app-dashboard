c6753503bf8f377d690fddc6e36da961e04a310d
Olivier Vernin <olivier.vernin@suse.com>
2022-10-22 20:00:09 +0200

    Add proxy configuration for dev environment
    Signed-off-by: Olivier Vernin <olivier.vernin@suse.com>

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  devServer: {
+      proxy: {
+        "^/api": {
+          target: 'http://localhost:8080',
+          ws: true,
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api' : '/'
+            },
+          },
+      },
+  },
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
