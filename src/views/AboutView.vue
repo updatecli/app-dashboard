10c838114bd8a27b8aeabd346cbb44b0917ac7c2
Olivier Vernin <olivier.vernin@suse.com>
2022-11-01 19:30:20 +0100

    Add basic about text
    Signed-off-by: Olivier Vernin <olivier.vernin@suse.com>

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,9 +4,18 @@
     <SideNavigation/>
 
     <v-main>
-      <div class="about">
-        <h1>This is an about page</h1>
-      </div>
+      <v-container>
+        <v-row>
+          <v-col>
+            <h1>About</h1>
+            <p>
+              <pre class="text-body-1">
+                {{ description }}
+              </pre>
+            </p>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
 
     <ReleaseFooter/>
@@ -20,6 +29,17 @@ import HeadNavigation from '../components/HeadNavigation.vue';
 
 export default {
   name: 'DashboardView',
+  data: () => ({
+    description: `
+      Updatemonitor is a new type of release monitoring platform. It brings more flexibility by leveraging Updatecli.\n
+
+      Updatecli is a declarative dependency management tool.
+      The people behind the Updatecli project are Open-Source veterans working on many different projects such as Jenkins.\n
+      We build Updatecli to make our work easier by automating processes that could be daunting to do manually. This allows us to focus on what matters in our day-to-day work.\n
+
+      Now we use Updatecli to monitor the state of our project to quickly identify version drift.
+    `
+}),
   components: {
     ReleaseFooter,
     SideNavigation,
