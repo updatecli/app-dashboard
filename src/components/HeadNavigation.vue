06d3b602029279b7cb481fd40c9c0a2e17c95dca
Olivier Vernin <olivier.vernin@suse.com>
2022-11-03 18:54:24 +0100

    Remove icon from top navigation bar
    Signed-off-by: Olivier Vernin <olivier.vernin@suse.com>

--- src/components/HeadNavigation.vue
+++ src/components/HeadNavigation.vue
@@ -1,7 +1,5 @@
 <template>
     <v-system-bar window app>
-        <v-btn icon="mdi-github"  variant="text" href="https://github.com/updatecli/app-dashboard" ></v-btn>
-        <v-btn icon="mdi-twitter"  variant="text" href="https://twitter.com/updatecliio" ></v-btn>
     </v-system-bar>
 </template>
 
