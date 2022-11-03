c80ae908995454d0872ede52bed84b64995e2677
Olivier Vernin <olivier.vernin@suse.com>
2022-11-03 18:56:59 +0100

    Add contact information to footer
    stick footer to bottom page
    
    Signed-off-by: Olivier Vernin <olivier.vernin@suse.com>

--- src/components/ReleaseFooter.vue
+++ src/components/ReleaseFooter.vue
@@ -1,13 +1,19 @@
 <template>
     <v-footer
         class="text-center d-flex flex-column"
+        app
+        bottom
+        fixed
+        padless
     >
         <div>
-            <v-btn class="mx-4" icon="mdi-github"  variant="text" href="https://github.com/updatecli/app-dashboard"></v-btn>
-            <v-btn class="mx-4" icon="mdi-twitter" variant="text" href="https://twitter.com/updatecliio"></v-btn>
+            <v-btn class="mx-4" icon="mdi-github"  variant="text" href="https://github.com/updatecli"></v-btn>
+            <v-btn class="mx-4" icon="mdi-twitter" variant="text" href="https://twitter.com/0lblak"></v-btn>
+            <v-btn class="mx-4" icon="mdi-mastodon" variant="text" href="https://fosstodon.org/@olblak"></v-btn>
+            <v-btn class="mx-4" icon="mdi-linkedin" variant="text" href="https://www.linkedin.com/in/overnin/"></v-btn>
         </div>
         <div>
-            {{ new Date().getFullYear() }} — <strong>Olblak</strong>
+            {{ new Date().getFullYear() }} — <strong>© Vernin Olivier</strong>
         </div>
     </v-footer>
 </template>
